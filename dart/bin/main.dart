bc1b23c55d3743b0591acfdef15f573b92f243a6
_kitg <gerasimovkit@gmail.com>
2025-12-29 18:07:02 -0500

    feat: global config, logging, and chart routes integrated

--- dart/bin/main.dart
+++ dart/bin/main.dart
@@ -1,20 +1,30 @@
 import 'package:heart/core/response.dart';
+import 'package:heart/globals/config.dart';
+import 'package:heart/globals/logging.dart';
 import 'package:heart/middleware/authentication.dart';
 import 'package:heart/middleware/authenticator.dart';
 import 'package:heart/middleware/config.dart';
 import 'package:heart/models/errors.dart';
 import 'package:heart/routes/charts.dart' as charts;
+import 'package:logging/logging.dart';
 import 'package:relic/io_adapter.dart';
 import 'package:relic/relic.dart';
 
-final _routes = <(String, Method), ModelHandler>{
+final _logger = Logger('API');
 
+final _routes = <(String, Method), ModelHandler>{
+  ('/charts', .get): charts.getChartPreferences,
+  ('/charts', .post): charts.saveChartPreference,
+  ('/charts/:preferenceId', .delete): charts.deleteChartPreference,
 };
 
 Future<void> main() async {
+  final config = AppConfig.fromEnv();
+  initLogging(config.logLevel, config.env);
+
   final app = RelicApp()
-    ..use('/', logRequests(logger: (msg, {stackTrace, type = .info}) => print(msg)))
-    ..use('/', configuration())
+    ..use('/', logRequests())
+    ..use('/', configuration(override: config))
     ..use('/', authenticator())
     ..use('/', authentication())
     ..fallback = respondWith((_) => JsonResponse.notFound());
@@ -32,8 +42,10 @@ Handler _handler(final ModelHandler handler) {
       final response = await handler(request);
       return JsonResponse.ok(body: response);
     } on ApiException catch (e) {
+      _logger.warning('API exception:', e);
       return JsonResponse(e.statusCode, body: e);
-    } catch (e) {
+    } catch (e, stackTrace) {
+      _logger.severe('API server error:', e, stackTrace);
       return JsonResponse.serverError();
     }
   };
