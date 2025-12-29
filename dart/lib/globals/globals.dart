b2183adcd35cbdd312e87eb7e2f37e4e149b8b4d
_kitg <gerasimovkit@gmail.com>
2025-12-29 15:53:08 -0500

    feat: Request user property and extension

---
+++ dart/lib/globals/globals.dart
@@ -0,0 +1,12 @@
+import 'package:heart/models/user.dart';
+import 'package:relic/relic.dart';
+
+final _userProperty = ContextProperty<User>('RequestUser');
+
+extension RequestUser on Request {
+  User get user => _userProperty.get(this);
+
+  String get userId => user.id;
+
+  set user(User u) => _userProperty[this] = u;
+}
