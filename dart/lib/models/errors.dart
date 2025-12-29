6f4712f226bb2d6acaec0628cab0ba72789fb2dd
_kitg <gerasimovkit@gmail.com>
2025-12-29 15:51:12 -0500

    feat: Model, User, and ApiException abstractions

---
+++ dart/lib/models/errors.dart
@@ -0,0 +1,31 @@
+import 'package:heart/models/model.dart';
+
+abstract class ApiException implements Model {
+  int get statusCode;
+}
+
+class NotFound implements ApiException {
+  final String type;
+  final String id;
+
+  const NotFound({
+    required this.type,
+    required this.id,
+  });
+
+  @override
+  String toString() {
+    return '$type #$id not found';
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      'error': 'not found',
+      'message': toString(),
+    };
+  }
+
+  @override
+  int get statusCode => 404;
+}
