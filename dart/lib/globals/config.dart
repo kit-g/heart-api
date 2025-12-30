74cd713b3509ec400321c384289b16ce0a5c5c44
_kitg <gerasimovkit@gmail.com>
2025-12-30 00:09:56 -0500

    feat: AWS-related fields and workouts table parsing in AppConfig

--- dart/lib/globals/config.dart
+++ dart/lib/globals/config.dart
@@ -25,11 +25,20 @@ abstract interface class AppConfig {
 
   String get logLevel;
 
+  String get awsRegion;
+
+  String get workoutsTable;
+
+  String? get awsProfile;
+
   factory AppConfig.fromEnv() {
     return _EnvConfig(
+      awsProfile: Platform.environment['AWS_PROFILE'],
+      awsRegion: Platform.environment['REGION']!,
       env: Env.fromString(Platform.environment['ENV']!),
       firebaseProjectId: Platform.environment['FIREBASE_PROJECT_ID']!,
       logLevel: Platform.environment['LOG_LEVEL'] ?? 'ALL',
+      workoutsTable: Platform.environment['WORKOUTS_TABLE']!,
     );
   }
 }
@@ -41,11 +50,20 @@ class _EnvConfig implements AppConfig {
   final String firebaseProjectId;
   @override
   final String logLevel;
+  @override
+  final String awsRegion;
+  @override
+  final String workoutsTable;
+  @override
+  final String? awsProfile;
 
   const _EnvConfig({
     required this.env,
     required this.firebaseProjectId,
     required this.logLevel,
+    required this.awsRegion,
+    required this.workoutsTable,
+    this.awsProfile,
   });
 }
 
