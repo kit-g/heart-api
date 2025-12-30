8499f7ed26202d5525d7ed4c5bdd77595469e56a
_kitg <gerasimovkit@gmail.com>
2025-12-30 00:10:42 -0500

    feat: routes centralized and database integration updated

--- dart/bin/main.dart
+++ dart/bin/main.dart
@@ -1,46 +1,37 @@
+import 'package:aws_client/dynamo_document.dart';
 import 'package:heart/core/response.dart';
+import 'package:heart/db/db.dart';
 import 'package:heart/globals/config.dart';
 import 'package:heart/globals/logging.dart';
 import 'package:heart/middleware/authentication.dart';
 import 'package:heart/middleware/authenticator.dart';
 import 'package:heart/middleware/config.dart';
+import 'package:heart/middleware/database.dart';
 import 'package:heart/models/errors.dart';
-import 'package:heart/routes/charts.dart' as charts;
+import 'package:heart/routes/index.dart';
 import 'package:logging/logging.dart';
 import 'package:relic/io_adapter.dart';
 import 'package:relic/relic.dart';
 
 final _logger = Logger('API');
 
-final _routes = <(String, Method), ModelHandler>{
-  ('/charts', .get): charts.getChartPreferences,
-  ('/charts', .post): charts.saveChartPreference,
-  ('/charts/:preferenceId', .delete): charts.deleteChartPreference,
+final config = AppConfig.fromEnv();
+
+final awsAuth = switch (config.awsProfile) {
+  String profile => AwsClientCredentials.fromProfileFile(profile: profile),
+  null => AwsClientCredentials.resolve(),
 };
 
-Future<void> main() async {
-  final config = AppConfig.fromEnv();
-  initLogging(config.logLevel, config.env);
-
-  final app = RelicApp()
-    ..use('/', logRequests())
-    ..use('/', configuration(override: config))
-    ..use('/', authenticator())
-    ..use('/', authentication())
-    ..fallback = respondWith((_) => JsonResponse.notFound());
-
-  for (final MapEntry(key: (route, verb), value: handler) in _routes.entries) {
-    app.add(verb, route, _handler(handler));
-  }
-
-  await app.serve();
-}
+final dynamo = DocumentClient(region: config.awsRegion, credentials: awsAuth);
+final database = Database(client: dynamo, table: config.workoutsTable);
 
 Handler _handler(final ModelHandler handler) {
   return (final Request request) async {
     try {
       final response = await handler(request);
       return JsonResponse.ok(body: response);
+    } on NoContent {
+      return JsonResponse.noContent();
     } on ApiException catch (e) {
       _logger.warning('API exception:', e);
       return JsonResponse(e.statusCode, body: e);
@@ -49,4 +40,22 @@ Handler _handler(final ModelHandler handler) {
       return JsonResponse.serverError();
     }
   };
+}
+
+Future<void> main() async {
+  initLogging(config.logLevel, config.env);
+
+  final app = RelicApp()
+    ..use('/', logRequests())
+    ..use('/', configuration(override: config))
+    ..use('/', authenticator())
+    ..use('/', authentication())
+    ..use('/charts', chartsDb(db: database))
+    ..fallback = respondWith((_) => JsonResponse.notFound());
+
+  for (final MapEntry(key: (route, verb), value: handler) in routes.entries) {
+    app.add(verb, route, _handler(handler));
+  }
+
+  await app.serve();
 }
