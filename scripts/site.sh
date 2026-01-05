48be15c9c42a3d3f2db76533584454cc6e434d62
_kitg <gerasimovkit@gmail.com>
2026-01-05 01:14:58 -0500

    feat: Apple and Android association files added to site deployment [skip ci]

--- scripts/site.sh
+++ scripts/site.sh
@@ -4,6 +4,10 @@ DISTRIBUTION_ID=$3
 
 aws s3 sync infrastructure/media/site "s3://$BUCKET/site" --delete --profile "$PROFILE"
 
+aws s3 cp infrastructure/media/site/.well-known/apple-app-site-association \
+  "s3://$BUCKET/site/.well-known/apple-app-site-association" \
+  --content-type application/json --profile "$PROFILE"
+
 aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" \
   --paths "/*" \
   --profile "$PROFILE" \
