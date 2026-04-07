bac7c21947a7eda5fa07ef051d9e4e00cf2b22f9
_kitg <gerasimovkit@gmail.com>
2026-04-06 21:54:44 -0400

    feat: connections middleware and context extension

--- api_v2/lib/middleware/database.dart
+++ api_v2/lib/middleware/database.dart
@@ -1,19 +1,34 @@
+import 'package:heart/models/connections.dart';
 import 'package:heart_models/heart_models.dart';
 import 'package:relic/relic.dart';
 
-final _property = ContextProperty<ChartPreferenceService>('ChartPreferenceService');
+final _chatsProperty = ContextProperty<ChartPreferenceService>('ChartPreferenceService');
+final _connectionsProperty = ContextProperty<ConnectionsService>('ConnectionsService');
 
 Middleware chartsDb({required ChartPreferenceService db}) {
   return (final Handler next) {
     return (final request) {
-      _property[request] = db;
+      _chatsProperty[request] = db;
+      return next(request);
+    };
+  };
+}
+
+Middleware connectionsDb({required ConnectionsService db}) {
+  return (final Handler next) {
+    return (final request) {
+      _connectionsProperty[request] = db;
       return next(request);
     };
   };
 }
 
 extension DatabaseContext on Request {
-  ChartPreferenceService get chartPreferenceService => _property.get(this);
+  ChartPreferenceService get chartPreferenceService => _chatsProperty.get(this);
 
-  set chartPreferenceService(ChartPreferenceService v) => _property[this] = v;
+  set chartPreferenceService(ChartPreferenceService v) => _chatsProperty[this] = v;
+
+  ConnectionsService get connectionsService => _connectionsProperty.get(this);
+
+  set connectionsService(ConnectionsService v) => _connectionsProperty[this] = v;
 }
