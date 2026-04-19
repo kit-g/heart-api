4b11187235b8090c4d8a50c5893554fd3e01104c
_kitg <gerasimovkit@gmail.com>
2026-04-19 15:43:47 -0400

    feat: profile upsert logic in database

---
+++ api_v2/lib/db/profiles.dart
@@ -0,0 +1,15 @@
+part of 'db.dart';
+
+mixin _Profiles on _DatabaseBase implements ApiProfileService {
+  @override
+  Future<void> upsertProfile(User user) async {
+    await _pool.execute(
+      _updateProfile.toSql(),
+      parameters: {
+        'id': user.id,
+        'email': user.email,
+        'username': user.displayName,
+      },
+    );
+  }
+}
