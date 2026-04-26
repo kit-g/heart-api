332cb724534fb9252f918c7ad065af4b8c155125
_kitg <gerasimovkit@gmail.com>
2026-04-26 15:36:41 -0400

    refactor: DynamoDB logic replaced with Postgres

--- api_v2/lib/db/db.dart
+++ api_v2/lib/db/db.dart
@@ -1,81 +1,29 @@
 library;
 
-import 'dart:convert';
-import 'dart:typed_data';
-
-import 'package:aws_client/dynamo_document.dart';
-import 'package:heart/models/av.dart';
 import 'package:heart/models/connections.dart';
 import 'package:heart/models/errors.dart';
+import 'package:heart/models/profile.dart';
+import 'package:heart/models/templates.dart';
+import 'package:heart/models/workouts.dart';
 import 'package:heart_models/heart_models.dart' hide WorkoutService, TemplateService;
-
-import '../models/profile.dart';
-import '../models/templates.dart';
-import '../models/workouts.dart';
+import 'package:postgres/postgres.dart' hide Connection;
 
 part 'charts.dart';
 part 'connections.dart';
+part 'profiles.dart';
+part 'queries.dart';
 part 'templates.dart';
 part 'workouts.dart';
 
-const _userPk = 'USER#';
-const _connectionPk = _userPk;
-const _connectionSk = 'CONN#';
-const _workoutSk = 'WORKOUT#';
-const _templateSk = 'TEMPLATE#';
-const _templateShareSk = 'TEMPLATE_SHARE#';
-
 abstract class _DatabaseBase {
-  DocumentClient get _client;
-
-  const _DatabaseBase();
-
-  String connectionPk(String userId) => '$_connectionPk$userId';
-
-  String connectionSk(ConnectionRole role, ConnectionDomain domain, String targetId) {
-    return '$_connectionSk${role.value.toUpperCase()}#${domain.value.toUpperCase()}#$targetId';
-  }
-
-  String templatePk(String userId) => '$_userPk$userId';
-
-  String templateSk(String templateId) => '$_templateSk$templateId';
+  Pool get _pool;
 }
 
 class Database extends _DatabaseBase
-    with _Charts, _Connections, _Workouts, _Templates
-    implements ChartPreferenceService, ConnectionsService, ApiWorkoutService, ApiTemplateService {
+    with _Charts, _Connections, _Profiles, _Workouts, _Templates
+    implements ChartPreferenceService, ConnectionsService, ApiProfileService, ApiWorkoutService, ApiTemplateService {
   @override
-  final DocumentClient _client;
-  @override
-  final String table;
+  final Pool _pool;
 
-  const Database({
-    required DocumentClient client,
-    required this.table,
-  }) : _client = client;
-}
-
-AttributeValue toDynamoType(Object? v) {
-  return switch (v) {
-    String s => AttributeValue(s: s),
-    num n => AttributeValue(n: n.toString()),
-    bool b => AttributeValue(boolValue: b),
-    null => AttributeValue(nullValue: true),
-    Uint8List bytes => AttributeValue(b: bytes),
-    Map m => AttributeValue(m: m.map((k, v) => MapEntry(k, toDynamoType(v)))),
-    List l => AttributeValue(l: l.map((v) => toDynamoType(v)).toList()),
-    _ => throw ArgumentError('Unsupported DynamoDB type: ${v.runtimeType} - $v'),
-  };
-}
-
-extension on Map<String, dynamic> {
-  Map<String, AttributeValue> toAttributeValue() {
-    return map((k, v) => MapEntry(k, toDynamoType(v)));
-  }
-}
-
-extension on DynamoItem {
-  Map<String, AttributeValue> toAttributeValue() {
-    return toDynamoItem().toAttributeValue();
-  }
+  Database({required Pool pool}) : _pool = pool;
 }
