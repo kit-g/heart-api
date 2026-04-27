79125e8577a9dfb93be7b147bac7e3312a62538a
_kitg <gerasimovkit@gmail.com>
2026-04-26 21:16:01 -0400

    feat: API Lambda function for Terraform stack

---
+++ infrastructure/terraform/stacks/api/lambdas.tf
@@ -0,0 +1,33 @@
+resource "aws_lambda_function" "api" {
+  function_name    = var.name_prefix
+  description      = "Part of Heart: API function"
+  role             = module.api_role.role_arn
+  runtime          = "provided.al2023"
+  architectures    = ["arm64"]
+  handler          = "app.handler"
+  filename         = data.archive_file.api.output_path
+  source_code_hash = data.archive_file.api.output_base64sha256
+  memory_size      = 512
+  timeout          = 120
+  depends_on       = [aws_cloudwatch_log_group.api]
+
+  layers = [
+    "arn:aws:lambda:${var.region}:753240598075:layer:LambdaAdapterLayerArm64:25"
+  ]
+
+  environment {
+    variables = {
+      AWS_LAMBDA_EXEC_WRAPPER  = "/opt/bootstrap"
+      AWS_LWA_PORT             = 8080
+      AWS_LWA_REMOVE_BASE_PATH = "/v2"
+      ENV                      = "dev"
+      EXERCISE_BUCKET          = var.content_bucket.bucket
+      FIREBASE_PROJECT_ID      = var.firebase_project_id
+      LOG_LEVEL                = "ALL"
+      MEDIA_DISTRIBUTION       = "https://dev.media.heart-of.me"
+      MIN_APP_VERSION          = "1.0.0"
+      REGION                   = var.region
+      SUPPORTED_LOCALES        = "en,en_CA,ru"
+    }
+  }
+}
