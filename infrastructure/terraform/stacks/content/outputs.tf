66a1711102e21b6b20c7f9ed78dea7bfee3b734d
_kitg <gerasimovkit@gmail.com>
2026-04-26 21:14:41 -0400

    feat: content stack for Terraform

---
+++ infrastructure/terraform/stacks/content/outputs.tf
@@ -0,0 +1,3 @@
+output "content_bucket" {
+  value = aws_s3_bucket.content
+}
---
+++ infrastructure/terraform/stacks/content/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z]{2}-[a-z]+-[0-9]{1}$", var.region))
+    error_message = "Region must be a valid AWS region format (e.g., us-east-1, eu-west-2)."
+  }
+}
+
+variable "account_id" {
+  description = "AWS account ID"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[0-9]{12}$", var.account_id))
+    error_message = "Account ID must be a 12-digit number."
+  }
+}
+
