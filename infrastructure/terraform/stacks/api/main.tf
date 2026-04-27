33fa9a6b18942283d4812c0e63f7829aac1f21fd
_kitg <gerasimovkit@gmail.com>
2026-04-26 21:16:11 -0400

    feat: API Gateway with Lambda integration for Terraform stack

---
+++ infrastructure/terraform/stacks/api/main.tf
@@ -0,0 +1,63 @@
+resource "aws_scheduler_schedule_group" "accounts" {
+  name = "accounts"
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "heart-api"
+}
+
+resource "aws_lambda_permission" "api_invoke" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  authorization = "NONE"
+  http_method   = "ANY"
+  resource_id   = aws_api_gateway_resource.proxy.id
+}
+
+resource "aws_api_gateway_integration" "proxy" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    redeployment = sha1(
+      jsonencode(
+        [
+          aws_api_gateway_rest_api.api.id,
+          aws_api_gateway_resource.proxy.id,
+          aws_api_gateway_method.proxy.id,
+          aws_api_gateway_integration.proxy.id,
+        ]
+      )
+    )
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "v1" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "v1"
+}
