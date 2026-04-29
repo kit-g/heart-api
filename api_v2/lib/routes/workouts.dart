3e55821306dbf03b1bb4447ec346d2ef2ed71798
_kitg <gerasimovkit@gmail.com>
2026-04-28 23:02:00 -0400

    feat: workout CRUD endpoints implemented

--- api_v2/lib/routes/workouts.dart
+++ api_v2/lib/routes/workouts.dart
@@ -1,14 +1,16 @@
+import 'package:heart/core/request.dart';
 import 'package:heart/globals/config.dart';
 import 'package:heart/globals/globals.dart';
 import 'package:heart/middleware/database.dart';
 import 'package:heart/models/errors.dart';
 import 'package:heart/models/workouts.dart';
 import 'package:heart/routes/permissions.dart';
+import 'package:heart_models/heart_models.dart';
 import 'package:relic/relic.dart';
 
 const _limitParam = IntQueryParam('pageSize');
 
-Future<WorkoutListResponse> getTargetUserWorkouts(final Request request) async {
+Future<WorkoutResponse> getTargetUserWorkouts(final Request request) async {
   final workoutsDb = request.workoutsService;
   final connectionsDb = request.connectionsService;
   final targetUserId = request.pathParameters.raw[#targetUserId]!;
@@ -28,4 +30,44 @@ Future<WorkoutListResponse> getTargetUserWorkouts(final Request request) async {
     cursor: since,
     imageUrl: request.config.workoutImageUrl,
   );
+}
+
+Future<Workout> getWorkout(final Request request) async {
+  final workoutId = request.pathParameters.raw[#workoutId]!;
+  return request.workoutsService.getWorkout(
+    userId: request.userId,
+    workoutId: workoutId,
+    imageUrl: request.config.workoutImageUrl,
+  );
+}
+
+Future<Workout> createWorkout(final Request request) async {
+  final body = await request.json();
+  final workout = WorkoutRequest(userId: request.userId, body: body);
+  return request.workoutsService.createWorkout(
+    userId: request.userId,
+    body: workout,
+    imageUrl: request.config.workoutImageUrl,
+  );
+}
+
+Future<Workout> updateWorkout(final Request request) async {
+  final workoutId = request.pathParameters.raw[#workoutId]!;
+  final body = await request.json();
+  final workout = WorkoutRequest(userId: request.userId, body: body);
+  return request.workoutsService.updateWorkout(
+    userId: request.userId,
+    workoutId: workoutId,
+    body: workout,
+    imageUrl: request.config.workoutImageUrl,
+  );
+}
+
+Future<NoContent> deleteWorkout(final Request request) async {
+  final workoutId = request.pathParameters.raw[#workoutId]!;
+  await request.workoutsService.deleteWorkout(
+    userId: request.userId,
+    workoutId: workoutId,
+  );
+  throw const NoContent();
 }
