05215e8bcefe794aae55135274b601dabd898ccc
_kitg <gerasimovkit@gmail.com>
2026-05-03 00:03:30 -0400

    feat: image storage and gallery models

---
+++ api_v2/lib/models/images.dart
@@ -0,0 +1,67 @@
+import 'package:heart_models/heart_models.dart';
+
+abstract interface class ApiImageStorageService {
+  Future<PreSignedUrl> presignUpload({required String key, required String mimeType});
+
+  Future<void> deleteObject({required String key});
+}
+
+abstract interface class ApiImageDbService {
+  Future<GalleryResponse> getGallery({
+    required String userId,
+    required String Function(String) imageUrl,
+    String? cursor,
+    int? pageSize,
+  });
+
+  Future<WorkoutImage> recordImage({
+    required String userId,
+    required String workoutId,
+    required String key,
+    required String Function(String) imageUrl,
+  });
+
+  Future<void> deleteImageRecord({
+    required String userId,
+    required String workoutId,
+    required String key,
+  });
+}
+
+class PresignedUploadResponse implements Model {
+  final PreSignedUrl preSignedUrl;
+  final String destinationUrl;
+  final String key;
+
+  const PresignedUploadResponse({
+    required this.preSignedUrl,
+    required this.destinationUrl,
+    required this.key,
+  });
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      'url': preSignedUrl.url,
+      'destinationUrl': destinationUrl,
+      'key': key,
+      'fields': preSignedUrl.fields,
+    };
+  }
+}
+
+class GalleryResponse with Iterable<WorkoutImage> implements Model {
+  final List<WorkoutImage> images;
+  final String? cursor;
+
+  const GalleryResponse({required this.images, this.cursor});
+
+  @override
+  Iterator<WorkoutImage> get iterator => images.iterator;
+
+  @override
+  Map<String, dynamic> toMap() => {
+    'images': map((img) => img.toRow()).toList(),
+    'cursor': ?cursor,
+  };
+}
