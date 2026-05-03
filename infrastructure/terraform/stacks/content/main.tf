10e0ced7e1f316176fd41f43664a8177b32a2f63
_kitg <gerasimovkit@gmail.com>
2026-05-03 19:29:06 -0400

    feat: S3 bucket lifecycle policy

--- infrastructure/terraform/stacks/content/main.tf
+++ infrastructure/terraform/stacks/content/main.tf
@@ -11,6 +11,19 @@ resource "aws_s3_bucket" "content" {
   bucket = "${var.account_id}-${var.region}-content"
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "delete_raw_uploads" {
+  bucket = aws_s3_bucket.content.id
+
+  rule {
+    id     = "expire-uploads"
+    status = "Enabled"
+
+    filter { prefix = "uploads/" }
+
+    expiration { days = 1 }
+  }
+}
+
 resource "aws_s3_bucket" "static" {
   bucket = "${var.account_id}-${var.region}-static"
 }
