e4c779706997ea3c684dc5e56a6330a5999f932a
_kitg <gerasimovkit@gmail.com>
2026-05-05 23:59:13 -0400

    feat: account deletion management queries

--- api_v2/lib/db/profiles.dart
+++ api_v2/lib/db/profiles.dart
@@ -2,8 +2,8 @@ part of 'db.dart';
 
 mixin _Profiles on _DatabaseBase implements ApiProfileService {
   @override
-  Future<void> upsertProfile(User user) async {
-    await _pool.execute(
+  Future<void> upsertProfile(User user) {
+    return _pool.execute(
       _updateProfile.toSql(),
       parameters: {
         'id': user.id,
@@ -11,6 +11,38 @@ mixin _Profiles on _DatabaseBase implements ApiProfileService {
         'username': user.displayName,
         'avatar': user.remoteAvatar,
       },
+    );
+  }
+
+  @override
+  Future<void> scheduleAccountDeletion({
+    required String userId,
+    required String scheduleArn,
+    required DateTime scheduledAt,
+  }) {
+    return _pool.execute(
+      _scheduleAccountDeletion.toSql(),
+      parameters: {
+        'userId': userId,
+        'schedule': scheduleArn,
+        'scheduledAt': scheduledAt,
+      },
+    );
+  }
+
+  @override
+  Future<void> undoAccountDeletion({required String userId}) {
+    return _pool.execute(
+      _undoAccountDeletion.toSql(),
+      parameters: {'userId': userId},
+    );
+  }
+
+  @override
+  Future<void> deleteAccount({required String userId}) {
+    return _pool.execute(
+      _deleteAccount.toSql(),
+      parameters: {'userId': userId},
     );
   }
 }
