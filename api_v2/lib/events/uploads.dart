6c114640d1b8dbc621b472dc11ae13fde4a01f36
_kitg <gerasimovkit@gmail.com>
2026-05-09 15:39:24 -0400

    feat: image tagging and processing logic extended for workouts and avatars

--- api_v2/lib/events/uploads.dart
+++ api_v2/lib/events/uploads.dart
@@ -7,33 +7,53 @@ import 'package:heart/middleware/database.dart';
 import 'package:heart/middleware/s3.dart';
 import 'package:relic/relic.dart';
 
+
 Future<void> imageUpload(
   final Request request,
   String bucket,
   String uploadKey, {
   required void Function(Object error, [StackTrace? st]) onError,
 }) async {
-  try {
-    final tags = await request.imageStorageService.getObjectTagging(bucket, uploadKey);
-
-    final userId = tags['user-id'];
-    final workoutId = tags['workout-id'];
-    final imageId = tags['image-id'];
-
-    if (userId == null || workoutId == null || imageId == null) return;
-
-    final ext = uploadKey.contains('.') ? uploadKey.split('.').last : 'jpg';
-    final hash = sha256.convert(utf8.encode('$userId:$workoutId')).toString().substring(0, 16);
-    final destKey = 'workouts/$hash/$imageId.$ext';
-
-    await request.imageStorageService.copyObject(fromKey: uploadKey, toKey: destKey);
+  Future<void> copyImage(String destinationKey) async {
+    await request.imageStorageService.copyObject(fromKey: uploadKey, toKey: destinationKey);
     await request.imageStorageService.deleteObject(key: uploadKey);
-    await request.imageDbService.recordImage(
-      userId: userId,
-      workoutId: workoutId,
-      key: destKey,
-      imageUrl: request.config.workoutImageUrl,
-    );
+  }
+
+  final tags = await request.imageStorageService.getObjectTagging(bucket, uploadKey);
+
+  try {
+    switch (tags) {
+      // workout image
+      case {
+            'user-id': String userId,
+            'workout-id': String workoutId,
+            'image-id': String imageId,
+          }
+          when [userId, workoutId, imageId].every((attr) => attr.isNotEmpty):
+        final ext = uploadKey.contains('.') ? uploadKey.split('.').last : 'jpg';
+        final hash = sha256.convert(utf8.encode('$userId:$workoutId')).toString().substring(0, 16);
+        final destKey = 'workouts/$hash/$imageId.$ext';
+        await copyImage(destKey);
+        await request.imageDbService.recordImage(
+          userId: userId,
+          workoutId: workoutId,
+          key: destKey,
+          imageUrl: request.config.cdnAssetUrl,
+        );
+
+      // avatar
+      case {
+            'kind': 'avatar',
+            'user-id': String userId,
+          }
+          when userId.isNotEmpty:
+        final destKey = 'avatars/$userId';
+        await copyImage(destKey);
+        await request.profileService.updateAvatarUrl(
+          userId: userId,
+          avatarUrl: request.config.cdnAssetUrl(destKey),
+        );
+    }
   } on AWSHttpException catch (e, st) {
     onError(e, st);
   }
