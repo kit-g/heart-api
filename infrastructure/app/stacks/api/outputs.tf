5f908a9dc440600d9c25a69267b6ee6d70a44c2a
_kitg <gerasimovkit@gmail.com>
2026-05-10 19:22:18 -0400

    refactor: monitoring dashboards migrated to Terraform

--- infrastructure/app/stacks/api/outputs.tf
+++ infrastructure/app/stacks/api/outputs.tf
@@ -4,3 +4,19 @@ output "api" {
     stage_path  = aws_api_gateway_stage.v1.stage_name
   }
 }
+
+output "function_name" {
+  value = aws_lambda_function.api.function_name
+}
+
+output "api_gateway_name" {
+  value = aws_api_gateway_rest_api.api.name
+}
+
+output "events_queue_name" {
+  value = aws_sqs_queue.events.name
+}
+
+output "events_dlq_name" {
+  value = aws_sqs_queue.events_dlq.name
+}
--- infrastructure/app/stacks/cdn/outputs.tf
+++ infrastructure/app/stacks/cdn/outputs.tf
@@ -1,5 +1,6 @@
 output "media_distribution" {
   value = {
+    id             = aws_cloudfront_distribution.media.id
     domain_name    = aws_cloudfront_distribution.media.domain_name
     hosted_zone_id = aws_cloudfront_distribution.media.hosted_zone_id
   }
@@ -7,6 +8,7 @@ output "media_distribution" {
 
 output "web_distribution" {
   value = {
+    id             = aws_cloudfront_distribution.web.id
     domain_name    = aws_cloudfront_distribution.web.domain_name
     hosted_zone_id = aws_cloudfront_distribution.web.hosted_zone_id
   }
