9b0a5ac464b8c27bf983fcb89164bc468fa3bde6
_kitg <gerasimovkit@gmail.com>
2026-06-02 23:11:01 -0400

    feat: firebase app configs and setup for iOS/Android added

--- infrastructure/global/stack/firebase.tf
+++ infrastructure/global/stack/firebase.tf
@@ -9,7 +9,7 @@ terraform {
 
 data "aws_s3_object" "firebase_secret" {
   bucket = var.static_bucket
-  key    = "secrets/firebase-terraform-sa.json"
+  key    = "secrets/firebase/firebase-terraform-sa.json"
 }
 
 provider "google-beta" {
@@ -29,5 +29,47 @@ resource "google_firebase_project" "default" {
   provider   = google-beta
   project    = var.firebase_project_config.project_id
   depends_on = [google_project_service.firebase]
+}
+
+resource "google_firebase_apple_app" "ios" {
+  provider     = google-beta
+  project      = var.firebase_project_config.project_id
+  display_name = "Heart iOS App"
+  bundle_id    = var.firebase_project_config.ios_bundle_id
+  team_id      = var.firebase_project_config.appstore_team_id
+  depends_on   = [google_firebase_project.default]
+}
+
+resource "google_firebase_android_app" "android" {
+  provider     = google-beta
+  project      = var.firebase_project_config.project_id
+  display_name = "Heart Android App"
+  package_name = var.firebase_project_config.android_app_id
+  depends_on   = [google_firebase_project.default]
+}
+
+data "google_firebase_apple_app_config" "ios" {
+  provider = google-beta
+  app_id   = google_firebase_apple_app.ios.app_id
+}
+
+data "google_firebase_android_app_config" "android" {
+  provider = google-beta
+  app_id   = google_firebase_android_app.android.app_id
+}
+
+# we'll store the config files in S3 for now and both the app and CI will find it there
+resource "aws_s3_object" "ios_firebase_config" {
+  bucket       = var.static_bucket
+  key          = "secrets/firebase/GoogleService-Info.plist"
+  content      = base64decode(data.google_firebase_apple_app_config.ios.config_file_contents)
+  content_type = "application/xml"
+}
+
+resource "aws_s3_object" "android_firebase_config" {
+  bucket       = var.static_bucket
+  key          = "secrets/firebase/google-services.json"
+  content      = base64decode(data.google_firebase_android_app_config.android.config_file_contents)
+  content_type = "application/json"
 }
 
