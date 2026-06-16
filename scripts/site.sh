3f54c17b8882f4e05556c2568f1e8aea7e22bb52
_kitg <gerasimovkit@gmail.com>
2026-06-15 21:10:58 -0400

    feat: environment-specific handling for app site association file added

--- scripts/site.sh
+++ scripts/site.sh
@@ -1,10 +1,11 @@
 BUCKET=$1
 PROFILE=$2
 DISTRIBUTION_ID=$3
+ENV=$4
 
-aws s3 sync infrastructure/media/site "s3://$BUCKET/site" --delete --profile "$PROFILE"
+aws s3 sync site "s3://$BUCKET/site" --delete --profile "$PROFILE" --exclude ".well-known/*"
 
-aws s3 cp infrastructure/media/site/.well-known/apple-app-site-association \
+aws s3 cp "site/.well-known/$ENV/apple-app-site-association" \
   "s3://$BUCKET/site/.well-known/apple-app-site-association" \
   --content-type application/json --profile "$PROFILE"
 
