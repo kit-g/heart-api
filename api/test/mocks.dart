7cde6db9d3f73be7735ae048f8960b1a25f5cf9c
_kitg <gerasimovkit@gmail.com>
2026-07-28 01:32:50 -0400

    refactor: app initialization modularized and test coverage expanded

--- api/test/mocks.dart
+++ api/test/mocks.dart
@@ -1,14 +1,18 @@
+import 'package:heart/db/db.dart';
 import 'package:heart/globals/config.dart';
 import 'package:heart/middleware/events.dart';
 import 'package:heart/models/exercises.dart';
 import 'package:heart/models/images.dart';
 import 'package:heart/models/profile.dart';
 import 'package:heart/models/workouts.dart';
+import 'package:heart/storage/s3.dart';
 import 'package:heart_models/heart_models.dart' hide ExerciseService, WorkoutService, TemplateService;
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks([
   AppConfig,
+  Database,
+  Storage,
   ApiProfileService,
   ApiImageDbService,
   ApiImageStorageService,
