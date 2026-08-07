244fe00627bb65c0a8ff6469d69d4c557020ba06
_kitg <gerasimovkit@gmail.com>
2026-08-06 21:52:59 -0400

    feat: input parts for account, charts, exercises, and images

--- api/lib/inputs/inputs.dart
+++ api/lib/inputs/inputs.dart
@@ -1,16 +1,24 @@
 library;
 
+import 'dart:convert';
+
 import 'package:heart/core/request.dart';
 import 'package:heart/globals/config.dart';
 import 'package:heart/globals/globals.dart';
 import 'package:heart/models/errors.dart';
 import 'package:heart_models/heart_models.dart';
+import 'package:mime/mime.dart';
 import 'package:relic/relic.dart';
 
+part 'account.dart';
+part 'charts.dart';
 part 'comments.dart';
 part 'connections.dart';
 part 'devices.dart';
+part 'exercise_preferences.dart';
+part 'exercises.dart';
 part 'goals.dart';
+part 'images.dart';
 part 'pagination.dart';
 part 'parse.dart';
 part 'template_folders.dart';
